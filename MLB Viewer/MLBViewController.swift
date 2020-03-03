bd34361ebefd1c1e5fe805da488fdce1c8348e7f
Bryan Huss <bryan.huss@gmail.com>
2020-03-03 15:36:52 -0500

    Removed ViewController.swift. Added MLBViewController.swift

---
+++ MLB Viewer/MLBViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MLBViewController.swift
+//  MLB Viewer
+//
+//  Created by Bryan Huss on 3/3/20.
+//  Copyright © 2020 Bryan Huss. All rights reserved.
+//
+
+import UIKit
+
+class MLBViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .red
+    }
+}
--- MLB Viewer/SceneDelegate.swift
+++ MLB Viewer/SceneDelegate.swift
@@ -12,18 +12,21 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
-
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let _ = (scene as? UIWindowScene) else { return }
         
-        let window = UIWindow(frame: UIScreen.main.bounds)
-        let customViewController = UIViewController()
-        window.rootViewController = customViewController
-        self.window = window
-        window.makeKeyAndVisible()
+//        let window = UIWindow(frame: UIScreen.main.bounds)
+//        let mlbViewController = UIViewController()
+//        window.rootViewController = mlbViewController
+//        self.window = window
+//        window.makeKeyAndVisible()
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = MLBViewController()
+        window?.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
